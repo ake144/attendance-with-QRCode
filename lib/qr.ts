c3541d9f4bbaa3e5a6bf632523496b51fbc01a7d
ake <tamiratake@gmail.com>
2024-11-11 17:58:56 +0300

    qr  data configured

--- lib/qr.ts
+++ lib/qr.ts
@@ -4,7 +4,7 @@
 
 export const generateQrData = ({userID}:{userID:string}) => {
     const today = new Date().toISOString().split('T')[0]; // "YYYY-MM-DD"
-    return `https://your-domain.com/api/attendance/mark?userId=${userID}&date=${today}`;
+    return `https://nextjs-qr-attendance.vercel.app/api/attendance/mark?userId=${userID}&date=${today}`;
   };
 
 
