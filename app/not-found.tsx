9b19c1b4d5f238b1400ec0d42532ee0046cc0c49
ake <tamiratake@gmail.com>
2024-12-11 16:11:15 +0300

    not found page added

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 
 function Notfound() {
@@ -38,13 +39,13 @@ function Notfound() {
 
     <h1 className="mt-6 text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">Uh-oh!</h1>
 
-    <p className="mt-4 text-gray-500">We can't find that page.</p>
-    <a
+    <p className="mt-4 text-gray-500">We can&apos;t find that page.</p>
+    <Link
       href="/"
       className="mt-6 inline-block rounded bg-indigo-600 px-5 py-3 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring"
     >
       Go Back Home
-    </a>
+    </Link>
   </div>
 </div>
 
