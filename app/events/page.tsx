e970fefc5e85e1cfcaf38a4c00baa87c130eb411
ake <tamiratake@gmail.com>
2024-12-12 10:14:03 +0300

    mertadata

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,7 +1,15 @@
 import EventHero from "@/components/events/event-hero"
 import EventList from "@/components/events/event-list"
 import EventTop from "@/components/events/event-top"
+import { Metadata } from "next"
 
+
+export const metadata: Metadata = {
+    title: 'Events| YouGo City Church' ,
+    description: 'Learn Events of yougo church and be part of it',
+  }
+
+  
 const EventPage= () => {
     return(<>
     <EventTop  />
